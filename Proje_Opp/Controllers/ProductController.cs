3e0981aadbc593c6324878ae605658fce3a8035a
Muhammed Enes Başpınar <Menesbaspinar@hotmail.com>
2024-05-28 22:24:56 +0300

    Product index eklendi ve listeleme metodu yazıldı

--- Proje_Opp/Controllers/ProductController.cs
+++ Proje_Opp/Controllers/ProductController.cs
@@ -1,12 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Proje_Opp.AppContext;
+using Proje_Opp.Entity;
 
 namespace Proje_Opp.Controllers
 {
     public class ProductController : Controller
     {
+        Context context = new Context();
         public IActionResult Index()
         {
-            return View();
+            var values = context.Products.ToList();
+            return View(values);
         }
     }
 }
