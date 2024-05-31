6791affe05f4cde7979ffceb729141e48f8b9a6c
Muhammed Enes Başpınar <Menesbaspinar@hotmail.com>
2024-05-31 22:04:06 +0300

    IProductDal interfaci eklendi ve metodları yazıldı

---
+++ DemoProduct/DataAccessLayer/Abstract/IProductDal.cs
@@ -0,0 +1,20 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IProductDal
+    {
+        void Insert(Product p);
+
+        void Update(Product p);
+
+        void Delete(Product p);
+
+        List<Product> GetAll();
+    }
+}
