1bb6c2972ca748d2f7cd6ee640ad846e2527f7ed
Muhammed Enes Başpınar <Menesbaspinar@hotmail.com>
2024-05-31 20:07:07 +0300

    DbContext oluşturuldu ve ilk migraiton işlemi yapıldı

---
+++ DemoProduct/DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,26 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options) { }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer("Server=DESKTOP-HJS8A4F;Database=Product_Db;Trusted_Connection=True;Integrated Security=true;TrustServerCertificate=True");
+        //}
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Customer> Customers { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
