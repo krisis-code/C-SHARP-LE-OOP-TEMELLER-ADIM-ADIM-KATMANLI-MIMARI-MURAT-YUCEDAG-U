1f2eda1cd90f8c5fed5b6acd493850bf5dbbcb03
Muhammed Enes Başpınar <Menesbaspinar@hotmail.com>
2024-05-31 22:24:04 +0300

    ProductDal class oluşturuldu IProductDal dan implamente edildi context dependency injection yapıldı ve metodlar yazıldı

---
+++ DemoProduct/DataAccessLayer/Repositories/ProductDal.cs
@@ -0,0 +1,45 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class ProductDal : IProductDal
+    {
+        private readonly Context context;
+
+        public ProductDal(Context _context)
+        {
+            context = _context;
+        }
+
+       
+        public void Delete(Product p)
+        {
+            context.Remove(p);
+            context.SaveChanges();
+        }
+
+        public List<Product> GetAll()
+        {
+           return context.Products.ToList();
+        }
+
+        public void Insert(Product p)
+        {
+            context.Add(p);
+            context.SaveChanges();
+        }
+
+        public void Update(Product p)
+        {
+          context.Update(p);
+            context.SaveChanges();
+        }
+    }
+}
