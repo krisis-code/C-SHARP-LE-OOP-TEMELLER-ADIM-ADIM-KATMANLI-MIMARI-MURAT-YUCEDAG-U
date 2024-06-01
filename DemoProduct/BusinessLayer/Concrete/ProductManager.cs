13af2e97a3bd7fc2753f357943a0004fcfe53511
Muhammed Enes Başpınar <Menesbaspinar@hotmail.com>
2024-06-01 12:07:06 +0300

    ProductManager class oluşturuldu IProductService den interface inheritance edildi ve imlamente edildi

---
+++ DemoProduct/BusinessLayer/Concrete/ProductManager.cs
@@ -0,0 +1,47 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        private readonly IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public void TDelete(Product t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product TGetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> TGetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TInsert(Product t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TUpdate(Product t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+}
