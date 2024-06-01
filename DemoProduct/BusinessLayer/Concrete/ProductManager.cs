c561cfddf7197827bf5a930cc46f23231470b4e4
Muhammed Enes Başpınar <Menesbaspinar@hotmail.com>
2024-06-01 12:14:22 +0300

    ProductManager metodarı yazıldı

--- DemoProduct/BusinessLayer/Concrete/ProductManager.cs
+++ DemoProduct/BusinessLayer/Concrete/ProductManager.cs
@@ -20,27 +20,27 @@ namespace BusinessLayer.Concrete
 
         public void TDelete(Product t)
         {
-            throw new NotImplementedException();
+           _productDal.Delete(t);
         }
 
         public Product TGetById(int id)
         {
-            throw new NotImplementedException();
+            return _productDal.GetById(id);
         }
 
         public List<Product> TGetList()
         {
-            throw new NotImplementedException();
+            return _productDal.GetList();
         }
 
         public void TInsert(Product t)
         {
-            throw new NotImplementedException();
+            _productDal.Insert(t);
         }
 
         public void TUpdate(Product t)
         {
-            throw new NotImplementedException();
+            _productDal.Update(t);
         }
     }
 }
