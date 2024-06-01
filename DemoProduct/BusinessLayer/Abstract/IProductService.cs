db6a55695158a36f91f6d2d93a397f7b92f06575
Muhammed Enes Başpınar <Menesbaspinar@hotmail.com>
2024-06-01 11:56:51 +0300

    IProductService interface oluşturuldu IGenericService den interface inheritance edildi

--- DemoProduct/BusinessLayer/Abstract/IProductService.cs
+++ DemoProduct/BusinessLayer/Abstract/IProductService.cs
@@ -7,31 +7,8 @@ using System.Threading.Tasks;
 
 namespace BusinessLayer.Abstract
 {
-    public class IProductService : IGenericService<Product>
+    public interface IProductService : IGenericService<Product>
     {
-        public void TDelete(Product t)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Product TGetById(int id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public List<Product> TGetList()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void TInsert(Product t)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void TUpdate(Product t)
-        {
-            throw new NotImplementedException();
-        }
+      
     }
 }
