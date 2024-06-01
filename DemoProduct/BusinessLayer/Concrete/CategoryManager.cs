011187e66fa1252d3ac2b9d1d7a257ade9f99615
Muhammed Enes Başpınar <Menesbaspinar@hotmail.com>
2024-06-01 12:10:34 +0300

    CategoryManager class  oluşturuldu ICategoryDal den interface inheritance edildi ve imlamente edildi

---
+++ DemoProduct/BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,45 @@
+﻿using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager : ICategoryDal
+    {
+        private readonly ICategoryDal _categoryDal;
+
+        public CategoryManager(ICategoryDal categoryDal)
+        {
+            _categoryDal = categoryDal;
+        }
+
+        public void Delete(Category t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Category GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Category> GetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Category t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Category t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
