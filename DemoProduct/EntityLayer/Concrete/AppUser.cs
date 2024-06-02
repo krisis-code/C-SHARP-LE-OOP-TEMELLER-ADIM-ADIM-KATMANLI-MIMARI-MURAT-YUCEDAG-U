64d87b8f8600d203ed76db0bcedb3c44850850a3
Muhammed Enes Başpınar <Menesbaspinar@hotmail.com>
2024-06-02 17:34:39 +0300

    AppRole ve AppUser proplar eklendi

--- DemoProduct/EntityLayer/Concrete/AppUser.cs
+++ DemoProduct/EntityLayer/Concrete/AppUser.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,12 @@ using System.Threading.Tasks;
 
 namespace EntityLayer.Concrete
 {
-    public class AppUser
+    public class AppUser : IdentityUser<int>
     {
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string Sender { get; set; }
     }
 }
