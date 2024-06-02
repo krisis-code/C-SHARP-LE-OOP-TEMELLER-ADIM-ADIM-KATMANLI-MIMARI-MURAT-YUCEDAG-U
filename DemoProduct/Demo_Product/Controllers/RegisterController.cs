71e25eca02cf02ec578887ef7ca9027d5973a2e7
Muhammed Enes Başpınar <Menesbaspinar@hotmail.com>
2024-06-02 17:44:40 +0300

    UserRegisterViewModel ekeldni

--- DemoProduct/Demo_Product/Controllers/RegisterController.cs
+++ DemoProduct/Demo_Product/Controllers/RegisterController.cs
@@ -1,9 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Demo_Product.Controllers
 {
     public class RegisterController : Controller
     {
+        private readonly UserManager<AppUser> _userManager;
+
+        public RegisterController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
         public IActionResult Index()
         {
             return View();
