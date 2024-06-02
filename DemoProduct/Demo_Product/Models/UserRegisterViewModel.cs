34960e1c1f49903f45a6ad1113dd3c6627879719
Muhammed Enes Başpınar <Menesbaspinar@hotmail.com>
2024-06-02 17:49:38 +0300

    UserRegisterViewModel proplar ve validations lar eklendi

--- DemoProduct/Demo_Product/Models/UserRegisterViewModel.cs
+++ DemoProduct/Demo_Product/Models/UserRegisterViewModel.cs
@@ -1,6 +1,23 @@
-﻿namespace Demo_Product.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Demo_Product.Models
 {
     public class UserRegisterViewModel
     {
+        [Required(ErrorMessage = "Lütfen isim giriniz")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Lütfen Soyisim giriniz")]
+        public string SurName { get; set; }
+
+        [Required(ErrorMessage = "Lütfen Kullanıcı giriniz")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Lütfen Şifre giriniz")]
+        public String Password { get; set; }
+
+        [Required(ErrorMessage = "Lütfen şifreyi tekrar giriniz")]
+        [Compare("Password",ErrorMessage = "Lütfen şifrelerin eşleştiğinden emin olunuz")]
+        public string ConfirmPassword { get; set; }
     }
 }
