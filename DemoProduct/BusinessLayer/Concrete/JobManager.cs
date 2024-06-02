11b2575bd54b8a39d70c19d5f64c4ab9ae4e4f68
Muhammed Enes Başpınar <Menesbaspinar@hotmail.com>
2024-06-02 13:13:32 +0300

    JobManager eklendi ve metodlar yazıldı

---
+++ DemoProduct/BusinessLayer/Concrete/JobManager.cs
@@ -0,0 +1,49 @@
+﻿using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    internal class JobManager : IJobDal
+    {
+        private readonly IJobDal _jobDal;
+
+        public JobManager(IJobDal jobDal)
+        {
+            _jobDal = jobDal;
+        }
+
+        public void Delete(Job t)
+        {
+           _jobDal.Delete(t);
+            
+        }
+
+        public Job GetById(int id)
+        {
+           var values = _jobDal.GetById(id);    
+            return values;
+        }
+
+        public List<Job> GetList()
+        {
+         ;
+            return _jobDal.GetList();
+        }
+
+        public void Insert(Job t)
+        {
+           _jobDal.Insert(t);
+
+        }
+
+        public void Update(Job t)
+        {
+            _jobDal.Update(t);
+        }
+    }
+}
