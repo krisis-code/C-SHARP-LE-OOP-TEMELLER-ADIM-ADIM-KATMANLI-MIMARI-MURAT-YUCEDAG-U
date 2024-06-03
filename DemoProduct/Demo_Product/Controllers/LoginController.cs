c6d6b5fcc395620506f05ef5dce845b301544a4b
Muhammed Enes Başpınar <Menesbaspinar@hotmail.com>
2024-06-03 20:52:48 +0300

    Login metodları yazıldı

--- DemoProduct/Demo_Product/Controllers/LoginController.cs
+++ DemoProduct/Demo_Product/Controllers/LoginController.cs
@@ -1,12 +1,44 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Demo_Product.Models;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Demo_Product.Controllers
 {
     public class LoginController : Controller
     {
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public LoginController(SignInManager<AppUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(UserLoginViewModel p)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(p.UserName, p.Password, false, true);
+            
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Hatalı kullanıcı adı yada şifre");
+                }
+            
+            }
+            return View();
+            
         }
     }
 }
