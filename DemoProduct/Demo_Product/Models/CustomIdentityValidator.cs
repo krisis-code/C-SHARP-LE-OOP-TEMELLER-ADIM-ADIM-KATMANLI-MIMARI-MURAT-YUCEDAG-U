faa711d01ce5411514ab9ee5d99af337a67edbe5
Muhammed Enes Başpınar <Menesbaspinar@hotmail.com>
2024-06-03 20:40:58 +0300

    CustomIdentityValidator custom mesajlar yazıldı

--- DemoProduct/Demo_Product/Models/CustomIdentityValidator.cs
+++ DemoProduct/Demo_Product/Models/CustomIdentityValidator.cs
@@ -1,6 +1,47 @@
-﻿namespace Demo_Product.Models
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Demo_Product.Models
 {
-    public class CustomIdentityValidator
+    public class CustomIdentityValidator : IdentityErrorDescriber
     {
+        public override IdentityError PasswordTooShort(int length)
+        {
+            return new IdentityError()
+            {
+                Code = "PasswordTooShort",
+                Description = "Parola en az 6 karakter içermelidir"
+            };
+        }
+
+        public override IdentityError PasswordRequiresLower()
+        {
+        
+            return new IdentityError()
+            {
+                Code = "PasswordRequiresLower",
+                Description = "Parola 1 küçük harf içermelidir"
+            };
+        }
+
+        public override IdentityError PasswordRequiresUpper()
+        {
+        
+
+            return new IdentityError()
+            {
+                Code = "PasswordRequiresUpper",
+                Description = "Parola 1 Büyük harf içermelidir"
+            };
+        }
+        public override IdentityError PasswordRequiresNonAlphanumeric()
+        {
+            return new IdentityError()
+            {
+                Code = "PasswordRequiresNonAlphanumeric",
+                Description = "Parola 1 özel harf içermelidir"
+            };
+        }
+
     }
+    
 }
