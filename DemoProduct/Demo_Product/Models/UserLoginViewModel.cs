de03323c6a900e6e02c0f5f2f83b01a5f78078af
Muhammed Enes Başpınar <Menesbaspinar@hotmail.com>
2024-06-03 20:46:35 +0300

    UserLoginViewModel proplar eklendi

--- DemoProduct/Demo_Product/Models/UserLoginViewModel.cs
+++ DemoProduct/Demo_Product/Models/UserLoginViewModel.cs
@@ -1,6 +1,13 @@
-﻿namespace Demo_Product.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Demo_Product.Models
 {
     public class UserLoginViewModel
     {
+        [Required(ErrorMessage ="Lütfen kullanıcı adınızı giriniz")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Lütfen şifrenizi giriniz")]
+        public string Password { get; set; }
     }
 }
