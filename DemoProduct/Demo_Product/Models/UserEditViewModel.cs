a8ab76ce5be4aa796fb25e900eabddf0e5896999
Muhammed Enes Başpınar <Menesbaspinar@hotmail.com>
2024-06-04 19:15:15 +0300

    UserEditViewModel eklendi

---
+++ DemoProduct/Demo_Product/Models/UserEditViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Demo_Product.Models
+{
+    public class UserEditViewModel
+    {
+        [Required(ErrorMessage = "Lütfen isim giriniz")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Lütfen Soyisim giriniz")]
+        public string SurName { get; set; }
+
+        [Required(ErrorMessage = "Lütfen Kullanıcı giriniz")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Lütfen mail giriniz")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Lütfen Şifre giriniz")]
+        public String Password { get; set; }
+
+        [Required(ErrorMessage = "Lütfen şifreyi tekrar giriniz")]
+        [Compare("Password",ErrorMessage = "Lütfen şifrelerin eşleştiğinden emin olunuz")]
+        public string ConfirmPassword { get; set; }
+    }
+}
