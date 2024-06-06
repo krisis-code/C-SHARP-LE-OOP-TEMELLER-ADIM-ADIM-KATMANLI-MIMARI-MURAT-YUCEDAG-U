f71e6461315b71ebf31e343492b1722004d72ab1
Muhammed Enes Başpınar <Menesbaspinar@hotmail.com>
2024-06-06 20:06:17 +0300

    Context klasörü eklendi , db bağlantı nesnesi oluşturuldu ve proplar eklendi

---
+++ Agriculture/DataAcccessLayer/Concrete/Context.cs
@@ -0,0 +1,32 @@
+﻿using Entitylayer.Concrete;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options) { }
+
+        
+
+        public DbSet<Adress> Adress { get; set; }
+
+        public DbSet<Contact> Contacts { get; set; }
+
+        public DbSet<Image> Images { get; set; }
+
+        public DbSet<News> News { get; set; }
+
+        public DbSet<Service> Services { get; set; }
+
+        public DbSet<Team> Teams { get; set; }
+
+    }
+}
