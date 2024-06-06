fc66e5b73e08a901757332ac376b99ae69e5ec64
Muhammed Enes Başpınar <Menesbaspinar@hotmail.com>
2024-06-06 19:47:06 +0300

    Team class eklendi ve proplar yazıldı

---
+++ Entitylayer/Concrete/Team.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entitylayer.Concrete
+{
+    public class Team
+    {
+        public int TeamId { get; set; }
+
+        public string  PersonelName { get; set; }
+
+        public string Title { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public string FacebookUrl { get; set; }
+
+        public string InstagramUrl { get; set; }
+        public string WebsiteUrl { get; set; }
+        public string TwitterUrl { get; set; }
+
+
+
+    }
+}
