4828980b87dc513ebd6823af95bfef9687f3fca5
Muhammed Enes Başpınar <Menesbaspinar@hotmail.com>
2024-06-11 22:49:33 +0300

    IImageDal interface eklendi

---
+++ Agriculture/DataAcccessLayer/Abstract/IImageDal.cs
@@ -0,0 +1,14 @@
+﻿using Entitylayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAcccessLayer.Abstract
+{
+    public interface IImageDal : IGenericDal<Image>
+    {
+      
+    }
+}
