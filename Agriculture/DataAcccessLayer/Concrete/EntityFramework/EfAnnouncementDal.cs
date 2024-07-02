ddaaaf5eb4cbec180ac5f7bd4d9e9bf4c00c6f45
Muhammed Enes Başpınar <Menesbaspinar@hotmail.com>
2024-07-03 00:05:54 +0300

    EfAnnouncementDal class ı eklendi

---
+++ Agriculture/DataAcccessLayer/Concrete/EntityFramework/EfAnnouncementDal.cs
@@ -0,0 +1,15 @@
+﻿using DataAcccessLayer.Abstract;
+using DataAcccessLayer.Concrete.Repository;
+using Entitylayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAcccessLayer.Concrete.EntityFramework
+{
+    public class EfAnnouncementDal : GenericRepository<Announcement>,IAnnouncementDal
+    {
+    }
+}
