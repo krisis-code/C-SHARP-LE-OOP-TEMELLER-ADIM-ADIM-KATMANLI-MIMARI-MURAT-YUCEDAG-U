d8e93fdcecb186626749d72583651557d5f110ff
Muhammed Enes Başpınar <Menesbaspinar@hotmail.com>
2024-08-31 08:44:01 +0300

    Admin teması eklendi

---
+++ Agriculture/AgriculturePresentaiton/Areas/Admin/Controllers/DasboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AgriculturePresentaiton.Areas.Admin.Controllers
+{
+    public class DasboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Agriculture/AgriculturePresentaiton/Program.cs
+++ Agriculture/AgriculturePresentaiton/Program.cs
@@ -25,8 +25,14 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapAreaControllerRoute(
+        name: "Admin",
+        areaName: "Admin",
+        pattern: "Admin/{controller=Dashboard}/{action=Index}/{id?}"
+        );
+    endpoints.MapDefaultControllerRoute();
+});
 
 app.Run();
